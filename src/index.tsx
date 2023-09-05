6e1b2ed14945294eefe23e8f5edf96bc339c1a3c
Jim Scarborough <jim.scarborough@clearspend.com>
2023-09-05 13:47:32 -0400

    remove unused import

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import 'react-tooltip/dist/react-tooltip.css'
 import { BrowserRouter as Router } from 'react-router-dom';

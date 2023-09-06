d7e6f50ef9487de2533d61966a08751fcfd90592
Jim Scarborough <jim.scarborough@clearspend.com>
2023-09-06 09:20:36 -0400

    add base to index.html

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container!);
+const homepage = process.env.REACT_APP_HOMEPAGE || '.';
 
 root.render(
   <React.StrictMode>
     <Router>
+      <base href={homepage} />
       <App />
     </Router>
   </React.StrictMode>
